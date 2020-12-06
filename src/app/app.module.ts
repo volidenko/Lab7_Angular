f3f0db4eb203f93505d1eb13e2dcb1aa02c3da8f
volidenko <volidenko@i.ua>
2020-12-06 17:54:07 +0200

    comm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
-import { PipesTestComponent } from './pipes-test/pipes-test.component';
 import { JoinPipe } from './join.pipe';
-import { HobbyComponent } from './hobby/hobby.component';
 import { ResumeComponent } from './resume/resume.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PipesTestComponent,
     JoinPipe,
-    HobbyComponent,
     ResumeComponent
   ],
   imports: [
--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Link } from 'src/link';
 import { Resume } from '../Resume';
 
 @Component({
@@ -8,12 +9,21 @@ import { Resume } from '../Resume';
 })
 export class ResumeComponent implements OnInit {
 resume: Resume;
+links: Link[]=[];
   constructor() { }
-
   ngOnInit(): void {
     this.resume =new Resume();
     this.resume.name="Hordiienko Volodymyr";
     this.resume.phone="+38 (097) 937-99-92";
     this.resume.city="Kropyvnytskyi";
+    this.resume.experience=11;
+    this.resume.photo="../../assets/images/dzhonni-depp.webp";
+    this.resume.skills;
+    this.resume.skills.push("Create ADO.NET");
+    this.resume.skills.push("Create WPF");
+    this.resume.skills.push("Create Angular");
+    this.links.push({url:"https://github.com/volidenko", text:"github"});
+    this.links.push({url:"https://www.facebook.com/profile.php?id=100015379385935", text:"facebook"});
+    this.links.push({url:"https://portal.azure.com/?quickstart=True#home", text:"azure"});
   }
 }
